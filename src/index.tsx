7241354ad8aba3a19d7c4bd761cffd065ef0feb8
Kirk Ampong <kirkampong2@gmail.com>
2022-05-25 06:13:56 -0400

    hard Router path to localhost

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
       <Routes>
-        <Route path="localhost:3000/#/" element={<App />}/>
+        <Route path="localhost:3000/#/" element={<App />} />
         <Route path="localhost:3000/#/about" element={<About />} />
       </Routes>
     </HashRouter>
